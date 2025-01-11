437c3094563ad076df899d6850c81071fe12a5cf
TahMeat <ron.nguyen@mavs.uta.edu>
2025-01-11 15:20:24 -0600

    Inital Kernel creation and Booting.
    [`kernel.c`, `kernel.ld`]
    + Implemented a basic kernel with a LIFO stack.

---
+++ kernel.c
@@ -0,0 +1,34 @@
+typedef unsigned char uint8_t;
+typedef unsigned int uint32_t;
+typedef uint32_t size_t;
+
+extern char __bss[], __bss_end[], __stack_top[];
+
+// Function to set our memory
+void *memset(void *buf, char c, size_t n){
+	uint8_t *p = (uint8_t *) buf;
+	while (n--)
+		*p++ = c;
+	return buf;
+}
+
+
+// Main kernal loop
+void kernel_main(void){
+	memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
+	
+	for (;;);
+}
+
+__attribute__((section(".text.boot")))
+__attribute__((naked))
+
+// on boot
+void boot(void){
+	__asm__ __volatile__(
+		"mv sp, %[stack_top]\n" // Set stack pointer
+		"j kernel_main\n" // Jump to our kernal main fuction
+		:
+		: [stack_top] "r" (__stack_top) // Pass the stack top address
+	);
+}
